dd537aff6a22a1a0823dd208230789bb9082ebc1
Pavel <panteleevofficial@yandex.ru>
2021-05-30 00:39:17 +0300

    AdvancedOOP ready :-)

--- src/main/java/lesson6_advancedOOP/Animals/DogPitBull.java
+++ src/main/java/lesson6_advancedOOP/Animals/DogPitBull.java
@@ -2,13 +2,13 @@ package lesson6_advancedOOP.Animals;
 
 public class DogPitBull extends Dog{
 
+    private static final String breed = "PitBull";
+
     public DogPitBull() {
         this.maxRunningLength = 400;
         this.maxSwimmingLength = 15;
         this.maxJumpingLength = 1;
     }
-
-    private static final String breed = "PitBull";
 
     public static String getBreed() {
         return breed;
--- src/main/java/lesson6_advancedOOP/Animals/DogShepherd.java
+++ src/main/java/lesson6_advancedOOP/Animals/DogShepherd.java
@@ -2,13 +2,13 @@ package lesson6_advancedOOP.Animals;
 
 public class DogShepherd extends Dog{
 
+    private static final String breed = "Shepherd";
+
     public DogShepherd() {
         this.maxRunningLength = 600;
         this.maxSwimmingLength = 10;
         this.maxJumpingLength = 1;
     }
-
-    private static final String breed = "Shepherd";
 
     public static String getBreed() {
         return breed;
