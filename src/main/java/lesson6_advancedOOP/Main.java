30eb3499cd78d2d35a267ab0ff630bf41299bac8
Pavel <panteleevofficial@yandex.ru>
2021-06-02 13:31:35 +0300

    Change main in 6 lessons to polimorfizm

--- src/main/java/lesson6_advancedOOP/Main.java
+++ src/main/java/lesson6_advancedOOP/Main.java
@@ -4,18 +4,16 @@ import lesson6_advancedOOP.animals.*;
 
 public class Main {
     public static void main(String[] args) {
-        CatLopEared cat1 = new CatLopEared(200, 0, 2);
-        CatTheBritish cat2 = new CatTheBritish(210, 0, 1.5);
-        DogPitBull dog1 = new DogPitBull();
-        DogShepherd dog2 = new DogShepherd();
 
-        System.out.println("Заставим делать упражнения вислоухого");
-        System.out.println(cat1.animalsPerformPhysicalEducation(1000, 1));
-        System.out.println("Заставим делать упражнения британского");
-        System.out.println(cat2.animalsPerformPhysicalEducation(1000, 1));
-        System.out.println("Дадим команду питбулю!");
-        System.out.println(dog1.animalsPerformPhysicalEducation(1000, 1));
-        System.out.println("Дадим команду Овчарке!");
-        System.out.println(dog2.animalsPerformPhysicalEducation(1000, 1));
+        Animal[] animal = new Animal[4];
+
+        animal[0] = new CatLopEared(200, 0, 2);
+        animal[1] = new CatTheBritish(210, 0, 1.5);
+        animal[2] = new DogPitBull();
+        animal[3] = new DogShepherd();
+
+        for (int i = 0; i < animal.length; i++) {
+            System.out.println(animal[i].animalsPerformPhysicalEducation(1000, 1));
+        }
     }
 }
